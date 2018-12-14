286acbeb113c8dd1ff804d43a67f325e9b1a41b4
Enea Dodi <dodi@chapman.edu>
2018-12-14 07:55:10 +0000

    Final Commit

--- sombrero.py
+++ sombrero.py
@@ -8,16 +8,14 @@
 # Course: PHYS220/MATH220/CPSC220 Fall 2018
 # Assignment: Cw12
 ###
-
+'''Sombrero module
+Module containg all the code for Cw12'''
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 import numba as nb
 from scipy.signal import argrelextrema
 from mpl_toolkits.mplot3d import Axes3D
-
-
-
 
 def dxdt(y):
     '''dxdt function
@@ -27,6 +25,12 @@ Returns: x'(t)'''
 
 @nb.jit
 def solve_odes(xVal,yVal,F,m=1,v=0.25,w=1,dt=0.001, T = 100*np.pi):
+    '''solve_odes method
+    args: xVal - inital x value
+          yVal - inital y value
+          F = driving force
+    returns: a pd.DataFrame containing np.arrays of
+    the points calcualated by FourthOrderRungeKutta.'''
     t = np.arange(0,T,dt)
     x = np.zeros_like(t, dtype= np.float64)
     y = np.zeros_like(t, dtype= np.float64)
@@ -34,7 +38,7 @@ def solve_odes(xVal,yVal,F,m=1,v=0.25,w=1,dt=0.001, T = 100*np.pi):
     y[0] = np.float64(yVal)
     #rt = np.array(x[0],y[0])
     for i in range(1,len(t)):
-        #rt = FourthOrderRungeKuttaR(dt,rt[0],rt[1],v,w,F,t)
+        #rt = FourthOrderRungeKuttaR(dt,rt,v,w,F,t,m)
         x[i] = FourthOrderRungeKuttaX(dt,y[i-1],x[i-1])
         y[i] = FourthOrderRungeKuttaY(dt,x[i-1],v,y[i-1],w,F,t[i-1])
         #x[i] = rt[0]
@@ -43,22 +47,26 @@ def solve_odes(xVal,yVal,F,m=1,v=0.25,w=1,dt=0.001, T = 100*np.pi):
     return returnDataframe
 
 @nb.jit
-def drdt(x,v,y,w,F,t):
-    return np.array([np.float64(y),(np.float64((-v*y)-x-(x**3)+F*np.cos(wt)))])
+def drdt(x,v,y,w,F,t,m):
+    '''drdt method
+    My attempt on creating a method which calculates both derivatives on the same time.'''
+    return np.array([np.float64(y),dydt(x,v,y,w,F,t)/m])
 
-'''
-def FourthOrderRungeKuttaR(dt,x,y,v,w,F,t):
-    K1r = dt*(drdt(x,v,y,w,F,t))
-    K2r = dt*((drdt(b,z,x,c)+np.array([K1r[0]/2,K1r[1]/2])).sum())
-    K3r = dt*((drdt(x,v,y,w,F,t)+np.array([K2r[0]/2,K2r[1]/2])).sum())
-    K4r = dt*((drdt(x,v,y,w,F,t)+np.array([K3r[0],K3r[1]])).sum()
-    valx = x +(K1r[0]+2*K2r[0]+2*K3r[0]+K4r[0])/6
-    valy = y + (K1r[1] + 2*K2r[1] + 2*K3r[1] + K4r[1])/6
-    return np.array([valx,valy])
-'''
+@nb.jit
+def FourthOrderRungeKuttaR(dt,r,v,w,F,t,m):
+    '''FourthOrderRungeKuttaXs
+    calculates the next x,y points using the RungeKuttaMethod'''
+    K1r = dt*(drdt(r[0],v,r[1],w,F,t,m))
+    K2r = dt*(drdt(r[0]+K1r[0]/2,v,r[1]+K1r[1]/2,w,F,(t+dt/2),m))
+    K3r = dt*(drdt(r[0]+K2r[0]/2,v,r[1]+K2r[1]/2,w,F,(t+dt/2),m))
+    K4r = dt*(drdt(r[0]+K1r[0],v,r[1]+K1r[1],w,F,(t+dt),m))
+    return (r + (K1r+2*K2r+2*K3r+K4)/6)
+
 
 @nb.jit
 def FourthOrderRungeKuttaX(dt,y,x0):
+    '''FourthOrderRungeKuttaXs
+    calculates the next x point using the RungeKuttaMethod'''
     K1x = dt*(dxdt(y))
     K2x = dt*(dxdt(y)+K1x/2)
     K3x = dt*(dxdt(y)+K2x/2)
@@ -68,22 +76,22 @@ def FourthOrderRungeKuttaX(dt,y,x0):
 @nb.jit
 def dydt(x,v,y,w,F,t):
     '''dydt function
-    Args: x - the value at x(t)
-      a - constant.
-      y - the value at y(t)
+    Args: x,v,y,w,F,t
     Returns: y'(t)'''
-    return np.float((-v*y)-x-(x**3)+F*np.cos(w*t))
+    return np.float((-v*y)+x-(x**3)+F*np.cos(w*t))
 
 @nb.jit
 def FourthOrderRungeKuttaY(dt,x,v,y,w,F,t):
+    '''FourthOrderRungeKuttaY
+    calculates the next y point using the RungeKuttaMethod'''
     K1y = dt*(dydt(x,v,y,w,F,t))
-    K2y = dt*(dydt(x,v,y,w,F,t)+K1y/2)
-    K3y = dt*(dydt(x,v,y,w,F,t)+K2y/2)
-    K4y = dt*(dydt(x,v,y,w,F,t)+K3y)
+    K2y = dt*(dydt(x,v,y,w,F,t+dt/2)+K1y/2)
+    K3y = dt*(dydt(x,v,y,w,F,t+dt/2)+K2y/2)
+    K4y = dt*(dydt(x,v,y,w,F,t+dt)+K3y)
     return y + (K1y+2*K2y+2*K3y+K4y)/6
               
 @nb.jit
-def ploty(sol):
+def ploty(sol,title):
     '''ploty function
 Args: sol - the panda dataframe
 Returns: Plots y v t'''
@@ -92,11 +100,11 @@ Returns: Plots y v t'''
     a.plot(sol["t"],sol["y"], color = "red")
     a.set_xlabel("t values")
     a.set_ylabel("y values")
-    a.set_title("y vs t")
+    a.set_title(title)
     plt.show()
 
 @nb.jit
-def plotx(sol):
+def plotx(sol,title):
     '''plotx function
 Args: sol - the panda dataframe
 Returns: Plots x v t'''
@@ -105,11 +113,11 @@ Returns: Plots x v t'''
     a.plot(sol["t"],sol["x"], color = "blue")
     a.set_xlabel("t values")
     a.set_ylabel("x values")
-    a.set_title("x vs t")
+    a.set_title(title)
     plt.show()
     
 @nb.jit
-def plotxy(sol):
+def plotxy(sol,title):
     '''plotxy function
 Args: sol - the panda dataframe
 Returns: Plots x v y'''
@@ -120,18 +128,19 @@ Returns: Plots x v y'''
     a.plot(xvals,yvals, color = "blue")
     a.set_xlabel("x values")
     a.set_ylabel("y values")
-    a.set_title("y vs x")
+    a.set_title(title)
     plt.show()
 
 @nb.jit
-def scatter(xVal,yVal,F,dt=0.001, N=50):
-    TVal = 2*np.pi
+def scatter(odes,F,title,dt=0.001, N=50):
+    '''scatter method
+    Args: odes - the pd.Dataframe containg the calculated results from solve_odes
+          title - title of the graph
+          F - driving Force
+    returns: a Poincare section'''
+    tVal = 2*np.pi
     fig = plt.figure()
-    for i in range(1,50):
-        odes = solve_odes(xVal,yVal,F,T = i*TVal)
-        if i == 0:
-            plt.scatter(xVal,yVal,marker=".", color = 'k')
-        else:
-            index = (int)(i/dt)
-            plt.scatter(odes["x"][:index],odes["y"][:index],marker ='.', color = 'k')
+    for i in range(0,N+1):
+        plt.scatter(odes["x"][(int)(tVal*i)],odes["y"][(int)(tVal*i)],marker='.',color = 'k')
+    plt.title(title)
     plt.show()
